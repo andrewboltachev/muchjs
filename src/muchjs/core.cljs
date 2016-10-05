f6223648e21b7f6beb236bd5c78ac361aee9fc9f
Andrei Boltachev <andreiboltachev@performancetelecom.co.uk>
2016-10-05 19:18:26 +0300

    some

--- src/muchjs/core.cljs
+++ src/muchjs/core.cljs
@@ -9,13 +9,20 @@
 
 (nodejs/enable-util-print!)
 
+(defn iprint [x]
+  (println)
+  (prn x)
+  (println)
+  x
+  )
+
 (def fs (nodejs/require "fs"))
 (def esprima (nodejs/require "esprima"))
 
 (defn transform1 [{:keys [type] :as obj}]
   (cond
     (and (= type "Program") (:sourceType obj) "script")
-    (map transform1 (:body obj))
+    (mapcat transform1 (:body obj))
 
     (and (= type "VariableDeclaration") (:kind obj) "var")
     (map transform1 (:declarations obj))
@@ -29,8 +36,46 @@
     (and (= type "Literal"))
     (:value obj)
 
+    (and (= type "IdentifierString"))
+    (:name obj)
+
     (and (= type "MemberExpression") (= (:type (:property obj)) "Identifier"))
     (list (symbol (str ".-" (:name (:property obj)))) (transform1 (:object obj)))
+
+    (and (= type "Identifier"))
+    (symbol (:name obj))
+
+    (and (= type "ObjectExpression"))
+    (into {}
+          (map (fn [nobj]
+                 (let [k (cond->
+                                    (:key nobj)
+
+                                    (= (:type (:key nobj)) "Identifier")
+                                    (assoc :type "IdentifierString")
+                                    )
+                       v (:value nobj)
+
+                       k (transform1 k)
+                       v (transform1 v)
+                       ]
+                   [k v]
+                   )
+                 )
+               )
+          (:properties obj)
+          )
+
+    (and (= type "FunctionExpression") (= (:type (:body obj)) "BlockStatement"))
+    (cons 'fn (cons (mapv transform1 (:params obj))
+          (transform1 (:body obj))
+          ))
+
+    (and (= type "BlockStatement"))
+    (map transform1 (:body obj))
+
+    (and (= type "ReturnStatement")) ; TODO: "check last"
+    (transform1 (:argument obj))
 
     :else
     (throw (js/Error. (str "Unsupported type " type)))
@@ -42,15 +87,21 @@
    source (str (.readFileSync fs "/home/andrey/example2.js"))
    parsed (.parse esprima source)
    data (js->clj (js/JSON.parse (js/JSON.stringify parsed)) :keywordize-keys true)
-   data (update-in data [:body] #(take 3 %))
+   data (update-in data [:body] #(take 5 %))
    ]
-  (prn data)
+  (println)
+  (println)
   (try
-    (prn (transform1 data))
+    (doseq [expr (transform1 data)]
+      (prn expr)
+      (println)
+      )
     (catch js/Error e
       (println "Error" e)
       )
     )
+  (println)
+  (println)
   (prn (sort (apply hash-set (map second (filter #(and (sequential? %) (= :type (first %))) (tree-seq #(or (map? %) (sequential? %)) identity data))))))
   #_(prn (into []
              (comp
